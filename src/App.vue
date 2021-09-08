b3681eb9e5c2f3717e3e212e908853485d2533e4
Mohamamd <and>
2021-09-08 20:57:27 +0430

    initial vue js

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseButton color="info" is-light="false" />
+  <BaseButton color="danger" is-light="false" is-large="large" />
+  <BaseButton color="warning" is-loading="true" is-light="false" is-large="large" />
+    <BaseButton color="primary" :is-loading="isLoading"  is-large="small" @click="changeColor" />
+</template>
+
+<script>
+
+import BaseButton from "./components/BaseButton.vue";
+
+export default {
+  name: 'App',
+  components: {
+    BaseButton
+  },
+
+  data(){
+    return {
+      isLoading :false
+    }
+  },
+
+  methods : {
+    changeColor(){
+      this.isLoading=true
+    }
+  }
+}
+</script>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button
+      class="button"  :class="{ [`is-${color}`]:color,
+      [`is-${isLarge}`] : isLight,
+      'is-large' : isLarge,
+      'is-loading': isLoading
+  }"
+  >Click</button>
+</template>
+
+<script>
+export default {
+name: "BaseButton",
+
+  props : {
+    color : {
+      type : String,
+      required : false
+    },
+    isLight:{
+      type:Boolean,
+      default : false
+    },
+    isLarge : {
+      type: String,
+      default : false
+    },
+    isLoading : {
+      type : String,
+      default : false
+    },
+  },
+
+
+}
+</script>
+
+<style lang="scss">
+  @import "bulma/sass/utilities/_all.sass";
+  @import "bulma/sass/elements/button.sass";
+</style>
