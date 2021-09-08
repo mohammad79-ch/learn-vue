24567b50f47a50e4f4aeadcc4eaa3beb74b018be
Mohamamd <and>
2021-09-08 23:56:47 +0430

    practic vue | component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
-  <BaseButton color="info" is-light="false" />
-  <BaseButton color="danger" is-light="false" is-large="large" />
-  <BaseButton color="warning" is-loading="true" is-light="false" is-large="large" />
-    <BaseButton color="primary" :is-loading="isLoading"  is-large="small" @click="changeColor" />
+<!--  <BaseButton color="info" is-light="false" />-->
+<!--  <BaseButton color="danger" is-light="false" is-large="large" />-->
+<!--  <BaseButton color="warning" is-loading="true" is-light="false" is-large="large" />-->
+<!--    <BaseButton color="primary" :is-loading="isLoading"  is-large="small" @click="changeColor" />-->
+
+<!--  base input -->
+
+  <BaseInput color="danger" />
+  <BaseInput color="primary" />
+  <BaseInput color="warning" v-model="age" />
+  <BaseInput color="warning" v-model="name" />
+  <BaseInput color="dark" :type="type" @click="showPassword" />
+  <p>{{name}}</p>
+
 </template>
 
 <script>
 
-import BaseButton from "./components/BaseButton.vue";
+// import BaseButton from "./components/BaseButton.vue";
+import BaseInput from "./components/BaseInput.vue";
 
 export default {
   name: 'App',
   components: {
-    BaseButton
+    // BaseButton
+    BaseInput
   },
 
   data(){
     return {
-      isLoading :false
+      isLoading :false,
+      name : "mamad",
+      age:21,
+      type : "password",
     }
   },
 
+
   methods : {
+    showPassword(){
+    if (this.type == 'password'){
+     return  this.type = 'text';
+    }
+    return this.type = 'password'
+    },
     changeColor(){
       this.isLoading=true
     }
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <input type="text" class="input is-rounded"
+  :class="{
+    [`is-${color}`]:color
+  }"
+  v-bind="$attrs"
+  :value="modelValue"
+  @input="(event) =>$emit('update:modelValue',event.target.value)"
+   >
+
+</template>
+
+<script>
+export default {
+  name : 'BaseInput',
+  inheritAttrs : false,
+  props : {
+    color:{
+      type:String,
+      required : false
+    },
+    modelValue : {
+      type : [String,Number]
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import "bulma/sass/utilities/_all.sass";
+@import "bulma/sass/helpers/spacing.sass";
+@import "bulma/sass/form/shared.sass";
+@import "bulma/sass/form/tools.sass";
+@import "bulma/sass/form/input-textarea.sass";
+</style>
