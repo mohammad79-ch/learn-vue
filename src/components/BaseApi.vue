d4837b131a599f2202054f83072cf75f9d6892ca
Mohamamd <and>
2021-09-10 23:51:17 +0430

    practice reactive and computed

--- src/components/BaseApi.vue
+++ src/components/BaseApi.vue
@@ -1,28 +1,42 @@
 <template>
-  <p>{{msg}}</p>
+  <p @click="handleName">{{user.name}}</p>
   <button @click="handleClick">Counter {{count}}</button>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive,computed} from 'vue'
 
 export default {
 
 name: "BaseApi",
 
   setup(){
-  let msg = "Mohammad Ch";
+
   let count = ref(0);
+
+  const user = reactive({name : "Mamads"})
 
   function handleClick(){
     count.value++
   }
 
+  const frameworks = reactive(['laravel','vue']);
+
+  const upperComputed = computed(()=> frameworks.map(framework => framework.toUpperCase()))
+
+    console.log(upperComputed)
+    function handleName(){
+     user.name = "Programmer"
+    }
+
   return{
-    msg,
     count,
-    handleClick
+    user,
+    upperComputed,
+    handleClick,
+    handleName,
   }
+
   }
 }
 </script>
